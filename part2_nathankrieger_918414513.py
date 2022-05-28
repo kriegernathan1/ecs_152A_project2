994f98e6cf8cc309b3f318af0bd28a2bb9e56201
Nathan Krieger <nkrieger0444@gmail.com>
2022-05-27 21:11:08 -0700

    wip

--- part2_nathankrieger_918414513.py
+++ part2_nathankrieger_918414513.py
@@ -108,6 +108,8 @@ def static_sliding_window():
                     print("Triple ack received, fast retransmission of packet #", last_ack_received_index + 1)
                     print("has ", number_of_acks_per_packet[last_ack_received_index], "acks")
                     s.sendto(all_packets[last_ack_received_index + 1].encode(), addr)
+
+                    print("The acks are", number_of_acks_per_packet)
                     
   
                 
